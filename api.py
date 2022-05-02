01bada4ed2e65b191ea1f733a6241391873bdec2
Alisson Martineli <martineli.alisson@gmail.com>
2022-05-02 19:48:45 -0300

    feat: get all todos

--- api.py
+++ api.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+todos = [
+    {
+        'id': 1,
+        'name': 'Learn Python',
+    },
+    {
+        'id': 2,
+        'name': 'Learn FastAPI',
+    }
+]
 
 
 @app.get("/")
 def hello_world():
     return {"message": "API running"}
+
+
+@app.get("/todos")
+def get_all():
+    return todos
